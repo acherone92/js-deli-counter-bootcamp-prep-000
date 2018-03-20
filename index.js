4beeaf06b44898d7e5b5a6fd21ffe2f3098eb75a
acherone92@gmail.com <acherone92@gmail.com>
2018-03-20 18:35:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, newCust){
-  katzDeliLine = katzDeli.push(newCust);
+  katzDeliLine.push(newCust);
   console.log(katzDeliLine);
   var position = parseInt(katzDeliLine.indexOf(newCust)) + 1;
   return `Welcome, ${newCust}. You are number ${position} in line.`;
